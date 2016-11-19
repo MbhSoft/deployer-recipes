121ead4a2a4a7d6816f389d059fde73d758d2b7d
Marc Bastian Heinrichs <typo3@mbh-software.de>
2016-11-19 23:46:19 +0100

    Add TYPO3 recipe with tasks
    * typo3cms:cache:flush_forced
    * typo3cms:database:updateschema
    * typo3cms:install:generatepackagestates

---
+++ recipes/typo3.php
@@ -0,0 +1,16 @@
+<?php
+
+// default path
+env('bin/typo3cms', 'bin/typo3cms');
+
+task('typo3cms:cache:flush_forced', function() {
+    run("{{bin/typo3cms}} cache:flush --force");
+})->desc('flush caches forced');
+
+task('typo3cms:database:updateschema', function() {
+    run("{{bin/typo3cms}} database:updateschema \"*.add,*.change\"");
+})->desc('update database schema');
+
+task('typo3cms:install:generatepackagestates', function() {
+    run("{{bin/typo3cms}} install:generatepackagestates");
+})->desc('generate packagestates file');
