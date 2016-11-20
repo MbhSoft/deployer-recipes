d9dd4070780166b45ec1594605a99a1c880ef879
Marc Bastian Heinrichs <typo3@mbh-software.de>
2016-11-20 01:04:28 +0100

    Make opcache tasks work

--- recipes/opcache.php
+++ recipes/opcache.php
@@ -1,36 +1,45 @@
 <?php
 
 env('web_dir', '.');
+env('public_url', '');
 
-set('reset_script_content', '
-<?php
+set(
+    'reset_script_content',
+    '<?php if (function_exists(\'opcache_reset\')) { opcache_reset(); @unlink(__FILE__); echo \'success\'; }'
+);
 
-if (function_exists("opcache_reset")) {
-    opcache_reset();
-}
-@unlink(__FILE__);
-echo "success";
-');
-
-function getResetScriptFileName() {
+function getResetScriptFileName()
+{
     $factory = new \RandomLib\Factory;
     $generator = $factory->getMediumStrengthGenerator();
     $scriptIdentifier = $generator->generateString(32, \RandomLib\Generator::CHAR_ALNUM);
     // store the $scriptIdentifier
     env('opcache_reset_scriptIdentifier', $scriptIdentifier);
 
-    $scriptFilename = 'surf-opcache-reset-' . $scriptIdentifier . '.php';
+    $scriptFilename = 'deployer-opcache-reset-' . $scriptIdentifier . '.php';
     return $scriptFilename;
 }
 
 task('opcache:reset:create_script', function() {
-    run("file_put_contents({{web_dir}}/" . getResetScriptFileName() . ",". str_replace('"', "\"", get('reset_script_content')) . ")");
+    run("echo \"" . get('reset_script_content') . "\" > {{web_dir}}/" . getResetScriptFileName());
 })->desc('create opcache reset script');
 
 task('local:opcache:reset:create_script', function() {
-    runLocally("file_put_contents({{web_dir}}/" . getResetScriptFileName() . ",". str_replace('"', "\"", get('reset_script_content')) . ")");
+    runLocally("echo \"" . get('reset_script_content') . "\" > {{web_dir}}/" . getResetScriptFileName());
 })->desc('create opcache reset script locally');
 
 task('opcache:reset:execute', function() {
-    run("curl -sS {{public_url}}/surf-opcache-reset-{{opcache_reset_scriptIdentifier}}.php");
+    run("curl -sS {{public_url}}/deployer-opcache-reset-{{opcache_reset_scriptIdentifier}}.php");
 })->desc('execute opcache reset script');
+
+task('local:opcache:reset:execute', function() {
+    runLocally("curl -sS {{public_url}}/deployer-opcache-reset-{{opcache_reset_scriptIdentifier}}.php");
+})->desc('execute opcache reset script locally');
+
+task('opcache:reset:remove_script', function() {
+    run("rm {{web_dir}}/deployer-opcache-reset-{{opcache_reset_scriptIdentifier}}.php");
+})->desc('remove opcache reset script');
+
+task('local:opcache:reset:remove_script', function() {
+    runLocally("rm {{web_dir}}/deployer-opcache-reset-{{opcache_reset_scriptIdentifier}}.php");
+})->desc('remove opcache reset script locally');
